d79f7d01396aca647a21d5c92f70b8e5922530b2
Alex <alex@wittyfinch.com>
2018-12-10 13:56:48 -0500

    typo

--- src/install/tools.sh
+++ src/install/tools.sh
@@ -4,13 +4,13 @@ set -e
 
 echo "Install some common packages"
 apt-get install -y \
-  && wget \
-  && curl \
-  && mousepad \
-  && net-tools \
-  && dnsutils \
-  && locales \
-  && bzip2
+  wget \
+  curl \
+  mousepad \
+  net-tools \
+  dnsutils \
+  locales \
+  bzip2
 
 echo "Generate locales for en_US.UTF-8"
 locale-gen en_US.UTF-8
