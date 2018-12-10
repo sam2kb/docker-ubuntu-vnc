b3dc409bd050424eae070abf8980fd621b66a660
Alex <alex@wittyfinch.com>
2018-12-10 13:34:21 -0500

    Yet another fix for --wait

--- src/scripts/vnc_startup.sh
+++ src/scripts/vnc_startup.sh
@@ -30,13 +30,13 @@ source $HOME/.bashrc
 
 # add `--skip` to startup args, to skip the VNC startup procedure
 if [[ $1 =~ -s|--skip ]]; then
-    echo -e "\n\n----------- SKIP VNC STARTUP"
-    echo -e "\n\n----------- EXECUTE COMMAND"
+    echo -e "\n\n==== SKIP VNC STARTUP"
+    echo -e "\n\n==== EXECUTE COMMAND"
     echo "Executing command: '${@:2}'"
     exec "${@:2}"
 fi
 if [[ $1 =~ -d|--debug ]]; then
-    echo -e "\n\n----------- DEBUG VNC STARTUP"
+    echo -e "\n\n==== DEBUG VNC STARTUP"
     export DEBUG=true
 fi
 
@@ -51,13 +51,13 @@ trap cleanup SIGINT SIGTERM
 VNC_IP=$(hostname -i)
 
 ## change vnc password
-echo -e "\n----------- change VNC password"
+echo -e "\n==== change VNC password"
 # first entry is control, second is view (if only one is valid for both)
 mkdir -p "$HOME/.vnc"
 PASSWD_PATH="$HOME/.vnc/passwd"
 
 if [[ -f $PASSWD_PATH ]]; then
-    echo -e "\n----------- purging existing VNC password settings"
+    echo -e "\n==== purging existing VNC password settings"
     rm -f $PASSWD_PATH
 fi
 
@@ -70,7 +70,7 @@ echo "$VNC_PW" | vncpasswd -f >> $PASSWD_PATH
 chmod 600 $PASSWD_PATH
 
 ## start vncserver
-echo -e "\n----------- start VNC server"
+echo -e "\n==== start VNC server"
 echo "remove old vnc locks to be a reattachable container"
 vncserver -kill $DISPLAY &> $STARTUPDIR/vnc_startup.log \
     || rm -rfv /tmp/.X*-lock /tmp/.X11-unix &> $STARTUPDIR/vnc_startup.log \
@@ -78,25 +78,19 @@ vncserver -kill $DISPLAY &> $STARTUPDIR/vnc_startup.log \
 
 echo -e "start vncserver with param: VNC_COL_DEPTH=$VNC_COL_DEPTH, VNC_RESOLUTION=$VNC_RESOLUTION\n..."
 if [[ $DEBUG == true ]]; then echo "vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION"; fi
-vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION &> $STARTUPDIR/vnc_startup.log &
+vncserver $DISPLAY -depth $VNC_COL_DEPTH -geometry $VNC_RESOLUTION &> $STARTUPDIR/vnc_startup.log
 echo -e "start window manager\n..."
-$HOME/wm_startup.sh &> /dev/null
+$HOME/wm_startup.sh &> $STARTUPDIR/wm_startup.log
 
 ## log connect options
-echo -e "\n\n----------- VNC environment started"
+echo -e "\n\n==== VNC environment started"
 echo -e "\nVNCSERVER started on DISPLAY= $DISPLAY \n\t=> connect via VNC viewer with $VNC_IP:$VNC_PORT"
 
-if [[ $DEBUG == true ]] || [[ $1 =~ -t|--tail-log ]]; then
-    echo -e "\n----------- $HOME/.vnc/*$DISPLAY.log"
-    # if option `-t` or `--tail-log` block the execution and tail the VNC log
-    tail -f $STARTUPDIR/*.log $HOME/.vnc/*$DISPLAY.log
-fi
-
 if [ -z "$1" ] || [[ $1 =~ -w|--wait ]]; then
-    wait $!
+    tail -f $STARTUPDIR/*.log $HOME/.vnc/*$DISPLAY.log
 else
     # unknown option ==> call command
-    echo -e "\n\n----------- EXECUTE COMMAND"
+    echo -e "\n\n==== EXECUTE COMMAND"
     echo "Executing command: '$@'"
     exec "$@"
 fi
